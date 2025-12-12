f7dc6f86e7a307a51170442e68939f8eda9a3b17
Saira Asif <sairaasif206@gmail.com>
2025-12-12 23:57:09 +0500

    first commit

--- textbook-site/docusaurus.config.js
+++ textbook-site/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://your-organization.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/humanoid-ai-textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
